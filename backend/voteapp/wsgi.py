b2d9cc2d74e03501121c185bb796ea6ea793fa49
cristian-rincon <cristian.o.rincon.b@gmail.com>
2022-02-01 19:37:39 -0500

    :construction: structure changes

--- backend/voteapp/wsgi.py
+++ backend/voteapp/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for App project.
+WSGI config for voteapp project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'voteapp.settings')
 
 application = get_wsgi_application()
