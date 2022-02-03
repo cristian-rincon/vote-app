298c6772cf154b7ff525d782132ec3f618d73635
cristian-rincon <cristian.o.rincon.b@gmail.com>
2022-02-03 18:57:40 -0500

    :sparkles: changes in admin

--- backend/polls/admin.py
+++ backend/polls/admin.py
@@ -2,5 +2,13 @@ from django.contrib import admin
 from .models import Question, Choice
 # Register your models here.
 
-admin.site.register(Question)
-admin.site.register(Choice)
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+
+
+@admin.register(Choice)
+class ChoiceAdmin(admin.ModelAdmin):
+    list_display = ('choice_text', 'votes')
+    list_filter = ('question',)
