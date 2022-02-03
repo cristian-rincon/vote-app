13a08d3125ecaaaa1089c8b763d46729341e251b
cristian-rincon <cristian.o.rincon.b@gmail.com>
2022-02-03 15:04:47 -0500

    :zap: current models added to admin view

--- backend/voteapp/settings.py
+++ backend/voteapp/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # My apps
+    'polls.apps.PollsConfig'
 ]
 
 MIDDLEWARE = [
