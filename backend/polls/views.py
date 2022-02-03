adeed00843ebb2547558e04381342fc2aa4c1cf9
cristian-rincon <cristian.o.rincon.b@gmail.com>
2022-02-03 17:39:58 -0500

    :art: function based views improved, in order to use class based views

--- backend/polls/views.py
+++ backend/polls/views.py
@@ -1,28 +1,35 @@
+from re import template
 import time
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
+from django.views import generic
 
 from .models import Question, Choice
 
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    return render(request, 'polls/index.html', {'latest_question_list': latest_question_list})
+class IndexView(generic.ListView):
+    """Return the last five published questions"""
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
 
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question': question})
+class DetailView(generic.DetailView):
+    """Return the detail of a question."""
+    model = Question
+    template_name = 'polls/detail.html'
 
-
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/results.html", {
-        "question": question
-    })
+class ResultView(generic.DetailView):
+    """Return the results of all questions."""
+    model = Question
+    template_name = 'polls/results.html'
 
 def vote(request, question_id):
+    """Handle voting for a question."""
     question = get_object_or_404(Question, pk=question_id)
     try:
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
