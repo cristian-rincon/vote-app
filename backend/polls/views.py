4f8bb27d682be6b5c9e5a9f7962697932e4f01b6
cristian-rincon <cristian.o.rincon.b@gmail.com>
2022-02-03 15:12:47 -0500

    :zap: function based views created

--- backend/polls/views.py
+++ backend/polls/views.py
@@ -2,5 +2,19 @@ import time
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 def index(request):
-    return HttpResponse(f"Hello, world. You're at the polls index. Time is {time.strftime('%X')}.")
+    return HttpResponse(f"You're at the polls index. Time is {time.strftime('%X')}.")
+
+
+def detail(request, question_id):
+    return HttpResponse(f"You're looking at question {question_id}.")
+
+
+def results(request, question_id):
+    response = f"You're looking at the results of question {question_id}."
+    return HttpResponse(response)
+
+
+def vote(request, question_id):
+    return HttpResponse(f"You're voting to question {question_id}.")
