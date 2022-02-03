82734ebccb1350b56a0b126d6700a74c3d2ad7c9
cristian-rincon <cristian.o.rincon.b@gmail.com>
2022-02-03 16:13:18 -0500

    :sparkles: basic templates added

--- backend/polls/views.py
+++ backend/polls/views.py
@@ -1,14 +1,18 @@
 import time
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+
+from .models import Question
 
 
 def index(request):
-    return HttpResponse(f"You're at the polls index. Time is {time.strftime('%X')}.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    return render(request, 'polls/index.html', {'latest_question_list': latest_question_list})
 
 
 def detail(request, question_id):
-    return HttpResponse(f"You're looking at question {question_id}.")
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 
 def results(request, question_id):
