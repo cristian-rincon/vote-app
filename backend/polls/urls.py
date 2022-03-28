06290b39fc59c130f33b55a967ab3348e05efe34
cristian-rincon <cristian.o.rincon.b@gmail.com>
2022-03-28 12:17:05 -0500

    :construction: apiv1 serializers,views refactored

--- backend/polls/urls.py
+++ backend/polls/urls.py
@@ -1,12 +1,7 @@
-from django.urls import path
+from rest_framework import routers
 
 from . import views
 
-app_name = 'polls'
-
-urlpatterns = [
-    path("", views.IndexView.as_view(), name="index"),
-    path("<int:pk>/", views.DetailView.as_view(), name="detail"),
-    path("<int:pk>/results/", views.ResultView.as_view(), name="results"),
-    path("<int:question_id>/vote/", views.vote, name="vote"),
-]
+router = routers.DefaultRouter()
+router.register(r"questions", views.QuestionViewSet)
+router.register(r"choices", views.ChoiceViewSet)
--- backend/voteapp/urls.py
+++ backend/voteapp/urls.py
@@ -15,12 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-from django.conf import settings
-from django.conf.urls.static import static
-
+from polls.urls import router as polls_router
+from users.urls import router as users_router
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path("polls/", include("polls.urls")),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
+    # Django Rest Framework
+    path("", include(users_router.urls)),
+    path("apiv1/", include(polls_router.urls)),
+    path("api-auth/", include("rest_framework.urls")),
+    path("admin/", admin.site.urls),
+    # path("polls/", include("polls.urls")),
+]  # + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
